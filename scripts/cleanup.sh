2fe2ae2e3385b70c5ed26b4266964ae2900d2bcd
lichen7567 <120573181@qq.com>
2026-05-19 03:50:44 +0800

    lichen7567 [ Fix ] #319
    Closes #319

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -35,7 +35,7 @@ done
 echo ""
 
 # Check total disk usage of log directories
-if [ $MAX_AGE_DAYS -gt $TOTAL_CLEANED ]; then
+if [ -z "${TOTAL_CLEANED}" ] || [ "${TOTAL_CLEANED}" -le "0" ] 2>/dev/null; then
     echo "WARNING: Retention period exceeds number of files cleaned"
     echo "Consider reducing MAX_AGE_DAYS (currently ${MAX_AGE_DAYS})"
 fi
