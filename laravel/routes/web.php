1b137e35b342c90bd207dd4708c3b2ae0d72de19
szamaniai <jakub.sniegocki@gmail.com>
2026-05-18 04:01:39 +0000

    MONAI: Solution for #785 - Solution for UnsafeLabs/Bounty-Hunters#785

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HealthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/health/database', [HealthController::class, 'database']);
