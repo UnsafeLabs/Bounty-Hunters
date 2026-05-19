bdfc292c5ad134866e0e40b095a2f46fc43700f7
정우철 <oocheol@naver.com>
2026-05-19 13:24:08 +0900

    [oocheol] [ FastAPI ] Add WebSocket heartbeat

--- fastapi/fastapi/websockets.py
+++ fastapi/fastapi/websockets.py
@@ -1,3 +1,92 @@
-from starlette.websockets import WebSocket as WebSocket  # noqa
+import asyncio
+import time
+from typing import Any, Callable, Optional
+
+from starlette.websockets import WebSocket as StarletteWebSocket
 from starlette.websockets import WebSocketDisconnect as WebSocketDisconnect  # noqa
 from starlette.websockets import WebSocketState as WebSocketState  # noqa
+
+class WebSocket(StarletteWebSocket):
+    pass
+
+class WebSocketWithHeartbeat:
+    def __init__(
+        self,
+        websocket: StarletteWebSocket,
+        ping_interval: float = 30.0,
+        pong_timeout: float = 10.0,
+        on_disconnect: Optional[Callable[[int, float], Any]] = None,
+    ):
+        self._websocket = websocket
+        self._ping_interval = ping_interval
+        self._pong_timeout = pong_timeout
+        self._on_disconnect = on_disconnect
+        self._start_time = time.time()
+        self._message_count = 0
+        self._pong_received = asyncio.Event()
+        self._heartbeat_task: Optional[asyncio.Task] = None
+
+    @property
+    def websocket(self) -> StarletteWebSocket:
+        return self._websocket
+
+    @property
+    def connection_duration(self) -> float:
+        return time.time() - self._start_time
+
+    @property
+    def message_count(self) -> int:
+        return self._message_count
+
+    async def accept(self, subprotocol: Optional[str] = None, headers: Optional[Any] = None) -> None:
+        await self._websocket.accept(subprotocol, headers)
+        self._heartbeat_task = asyncio.create_task(self._run_heartbeat())
+
+    async def send_text(self, data: str) -> None:
+        await self._websocket.send_text(data)
+
+    async def receive_text(self) -> str:
+        data = await self._websocket.receive_text()
+        self._message_count += 1
+        return data
+
+    async def send_json(self, data: Any, mode: str = "text") -> None:
+        await self._websocket.send_json(data, mode)
+
+    async def receive_json(self, mode: str = "text") -> Any:
+        data = await self._websocket.receive_json(mode)
+        self._message_count += 1
+        return data
+
+    async def send_bytes(self, data: bytes) -> None:
+        await self._websocket.send_bytes(data)
+
+    async def receive_bytes(self) -> bytes:
+        data = await self._websocket.receive_bytes()
+        self._message_count += 1
+        return data
+
+    async def close(self, code: int = 1000, reason: Optional[str] = None) -> None:
+        if self._heartbeat_task:
+            self._heartbeat_task.cancel()
+        await self._websocket.close(code, reason)
+        if self._on_disconnect:
+            self._on_disconnect(code, self.connection_duration)
+
+    async def _run_heartbeat(self):
+        try:
+            while self._websocket.client_state == WebSocketState.CONNECTED:
+                await asyncio.sleep(self._ping_interval)
+                
+                self._pong_received.clear()
+                try:
+                    await asyncio.wait_for(self._pong_received.wait(), timeout=self._pong_timeout)
+                except asyncio.TimeoutError:
+                    await self.close(code=1001, reason="Heartbeat timeout")
+                    break
+        except asyncio.CancelledError:
+            pass
+
+    def acknowledge_pong(self):
+        """Method to be called when a pong is received to reset the timer."""
+        self._pong_received.set()
