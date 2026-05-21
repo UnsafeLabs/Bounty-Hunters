16e42f710f4a1eaf8bc5161c5adf5e2447fe378a
Major126 <kkoge@qq.com>
2026-05-21 23:29:36 +0800

    [Laravel] Add rate limiting to web routes with debug endpoint

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('throttle:60,1')->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('/rate-limit-debug', function (Request $request) {
+        return response()->json([
+            'X-RateLimit-Limit'     => $request->headers->get('X-RateLimit-Limit'),
+            'X-RateLimit-Remaining' => $request->headers->get('X-RateLimit-Remaining'),
+            'X-RateLimit-Reset'     => $request->headers->get('X-RateLimit-Reset'),
+            'ip'                    => $request->ip(),
+            'user_id'               => auth()->id(),
+        ]);
+    })->name('rate-limit.debug');
 });
