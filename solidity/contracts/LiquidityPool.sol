abf34b5ec8d1525362c3f3a995932971c986702b
Gaotax2006 <zhuweiba9988@qq.com>
2026-05-21 22:09:05 +0800

    Fix #919: add pool drainage protection (solcjs 0.8.35)

--- solidity/contracts/LiquidityPool.sol
+++ solidity/contracts/LiquidityPool.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract LiquidityPool is ERC20 {
+    using SafeERC20 for IERC20;
+
     IERC20 public tokenA;
     IERC20 public tokenB;
 
