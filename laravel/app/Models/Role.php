29c1dee91fc7a4528745ca37a16f4bfdec4f3118
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-22 16:27:09 -0400

    fix: apply solution for issue #788

--- laravel/app/Models/Role.php
+++ laravel/app/Models/Role.php
@@ -2,31 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Role extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'name',
         'guard_name',
     ];
 
-    public function permissions(): BelongsToMany
-    {
-        return $this->belongsToMany(Permission::class, 'role_has_permissions');
-    }
-
-    public function givePermissionTo(Permission $permission): void
-    {
-        $this->permissions()->syncWithoutDetaching($permission);
-    }
-
-    public function revokePermissionTo(Permission $permission): void
-    {
-        $this->permissions()->detach($permission);
-    }
+    public $timestamps = false;
 }
