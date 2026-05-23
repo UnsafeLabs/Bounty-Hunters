9855586816b6c5bc5627b541092fd6b0aea160ee
kpoxo6op <kpoxo6op@gmail.com>
2026-05-23 23:05:01 +1200

    Add Laravel structured error logging

---
+++ laravel/app/Logging/JsonLogFormatter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Logging;
+
+use Monolog\Formatter\JsonFormatter;
+use Monolog\Logger;
+
+class JsonLogFormatter
+{
+    public function __invoke(Logger $logger): void
+    {
+        foreach ($logger->getHandlers() as $handler) {
+            $handler->setFormatter(
+                new JsonFormatter(JsonFormatter::BATCH_MODE_JSON, true)
+            );
+        }
+    }
+}
