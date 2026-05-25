cb8dc656fdac2ec6c0542da25e265144ad950f5f
高天翱翔 <zhuweiba9988@qq.com>
2026-05-25 08:36:07 +0800

    Fix #753: Add scheduled task registration and log cleanup command

--- laravel/routes/console.php
+++ laravel/routes/console.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('logs:clear', function () {
+    $logPath = storage_path('logs');
+    $files = glob($logPath . '/*.log');
+    foreach ($files as $file) {
+        unlink($file);
+    }
+    $this->info('Log files cleared!');
+})->purpose('Clear application log files');
+
+Schedule::command('logs:clear')->daily();
