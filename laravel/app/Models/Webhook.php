15ba23bfbcf261f8f05c6821db1575fe10908e91
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-25 07:13:57 -0400

    fix: apply solution for issue #754

--- laravel/app/Models/Webhook.php
+++ laravel/app/Models/Webhook.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Webhook extends Model
 {
@@ -21,14 +22,12 @@ class Webhook extends Model
         'active' => 'boolean',
     ];
 
-    public function deliveries()
+    protected $hidden = [
+        'secret',
+    ];
+
+    public function deliveries(): HasMany
     {
         return $this->hasMany(WebhookDelivery::class);
     }
-
-    public function isActive(): bool
-    {
-        return $this->active;
-    }
 }
-?>
