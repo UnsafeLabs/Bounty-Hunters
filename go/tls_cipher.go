51ace4a4692005fa40630fbc078e147cc2c0e3fa
高天翱翔 <zhuweiba9988@qq.com>
2026-05-26 20:52:29 +0800

    Gaotax2006 [ Go ] Fix #13: fix FilterWeakSuites missing RC4 and 3DES cip

--- go/tls_cipher.go
+++ go/tls_cipher.go
@@ -194,7 +194,10 @@ func (r *SuiteRegistry) FilterWeakSuites(suites []*CipherSuite) []*CipherSuite {
 
 	result := make([]*CipherSuite, 0, len(suites))
 	for _, s := range suites {
-		if s.KeySize >= minKeyBits {
+		if s.KeySize >= minKeyBits &&
+			!strings.Contains(s.Name, "RC4") &&
+			!strings.Contains(s.Name, "3DES") &&
+			!strings.Contains(s.Name, "DES") {
 			result = append(result, s)
 		}
 	}
