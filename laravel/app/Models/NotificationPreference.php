9f25f2bab6eca99d039f55731e8aa20c585d6802
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-27 11:44:46 -0400

    fix: apply solution for issue #793

--- laravel/app/Models/NotificationPreference.php
+++ laravel/app/Models/NotificationPreference.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class NotificationPreference extends Model
 {
@@ -20,8 +21,13 @@ class NotificationPreference extends Model
         'enabled' => 'boolean'
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function scopeEnabled($query)
+    {
+        return $query->where('enabled', true);
     }
 }
