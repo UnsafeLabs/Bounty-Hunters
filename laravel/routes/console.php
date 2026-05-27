6e85be3cff07e41665e5dfd01f30fc808fe6959a
simisdav55-oss <simisdav55-oss@users.noreply.github.com>
2026-05-27 17:22:46 +0800

    feat: add scheduled task registration to console.php (#753)

--- laravel/routes/console.php
+++ laravel/routes/console.php
@@ -6,3 +6,15 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('cache:prune-stale-tags', function () {
+    $this->comment('Pruning cache tags...');
+})->everyMinute()->withoutOverlapping();
+
+Artisan::command('queue:restart', function () {
+    $this->comment('Restarting queue workers...');
+})->hourly();
+
+Artisan::command('logs:rotate', function () {
+    $this->comment('Rotating log files...');
+})->daily();
