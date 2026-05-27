54559e2fb92f1666d3799bbb95b84ec0f02eb8c9
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-27 16:33:29 -0400

    fix: apply solution for issue #745

--- laravel/database/factories/UserFactory.php
+++ laravel/database/factories/UserFactory.php
@@ -19,19 +19,19 @@ class UserFactory extends Factory
 
     /**
      * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password', [
                 'rounds' => config('hashing.bcrypt.rounds', 10),
             ]),
             'remember_token' => Str::random(10),
         ];
     }
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
         ];
     }
 
