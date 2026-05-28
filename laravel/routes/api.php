6dca03b0afae098487a0b3b4080d6acafa2dee31
zhangjiayang6835-cyber <zhangjiayang6835@gmail.com>
2026-05-29 01:33:06 +0800

    feat(laravel): implement API authentication controller with Sanctum

---
+++ laravel/routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
