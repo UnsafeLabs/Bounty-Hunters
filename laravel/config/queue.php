11b8bcb67ebd1cc9f8d0d69af66831aa74155257
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-28 04:20:03 -0400

    fix: apply solution for issue #789

--- laravel/config/queue.php
+++ laravel/config/queue.php
@@ -34,11 +34,12 @@ return [
         'sync' => [
             'driver' => 'sync',
         ],
-
-        'database' => [
-            'driver' => 'database',
-            'connection' => env('DB_QUEUE_CONNECTION'),
             'table' => env('DB_QUEUE_TABLE', 'jobs'),
+            'queue' => env('DB_QUEUE', 'default'),
+            'retry_after' => (int) env('DB_QUEUE_RETRY_AFTER', 90),
+            'max_tries' => (int) env('DB_QUEUE_MAX_TRIES', 3),
+            'after_commit' => false,
+        ],
             'queue' => env('DB_QUEUE', 'default'),
             'retry_after' => (int) env('DB_QUEUE_RETRY_AFTER', 90),
             'after_commit' => false,
