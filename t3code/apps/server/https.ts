db973305afd9d37b4ab9e23bd021233a85a7a131
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-29 08:08:01 -0400

    fix: apply solution for issue #841

---
+++ t3code/apps/server/https.ts
@@ -0,0 +1,43 @@
+import Mime from "@effect/platform-node/Mite";
+import { decodeOtlpTraceRecords } from "@t3tools/shared/observability";
+import * as Data from "effect/Data";
+import * as Effect from "effect/Effect";
+import * as FileSystem from "effect/FileSystem";
+import * as Path from "effect/Path";
+import { cast } from "effect/Function";
+import {
+  HttpBody,
+  HttpClient,
+  HttpClientResponse,
+  HttpRouter,
+  HttpServerResponse,
+  HttpServerRequest,
+  HttpMiddleware,
+  HttpServerRespondable,
+  HttpServerResponse,
+  HttpServerRequest,
+  HttpMiddleware,
+  HttpServerRequest,
+  HttpServerResponse,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServerRequest,
+  HttpServer
