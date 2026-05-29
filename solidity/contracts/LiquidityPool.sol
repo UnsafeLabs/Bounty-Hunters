67d9181569889255d1fd387bbff25bfd4901e396
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-29 08:47:26 -0400

    fix: apply solution for issue #918

--- solidity/contracts/LiquidityPool.sol
+++ solidity/contracts/LiquidityPool.sol
@@ -82,48 +82,115 @@ contract LiquidityPool is ERC20 {
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract LiquidityPool is ERC20 {
+contract LiquidityPool is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
-    
-    IERC20 public token0;
-    IERC20 public token1;
-    
+
+    uint256 public constant MINIMUM_LIQUIDITY = 1000;
+
+    address public token0;
+    address public token1;
+
     uint256 public reserve0;
     uint256 public reserve1;
-    uint256 public constant MINIMUM_LIQUIDITY = 1000;
     
-    uint256 private unlocked = 1;
-    
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    event Deposit(address indexed sender, uint256 amount0, uint256 amount1);
+    event Withdraw(address indexed sender, uint256 amount0, uint256 amount1);
     event Sync(uint256 reserve0, uint256 reserve1);
-    event Mint(address indexed sender, uint256 amount0, uint256 amount1);
-    event Burn(address indexed sender, uint256 amount0, uint2025 amount1, address indexed to);
-    event Swap(address indexed sender, uint256 amount0In, uint256 amount1In, uint256 amount0Out, uint256 amount1Out, address indexed to);
-    
+
     constructor(address _token0, address _token1) {
-        token0 = IERC20(_token0);
-        token1 = IERC20(_token1);
+        token0 = _token0;
+        token1 = _token1;
     }
-    
-    function addLiquidity(address to, uint256 amount0, uint256 amount1) public returns (uint256 liquidity) {
-        // Implementation would go here
+
+    function deposit(uint256 amount0, uint256 amount1) external nonReentrant returns (uint256 liquidity) {
+        (uint256 _reserve0, uint256 _reserve1) = (reserve0, reserve1);
+        uint256 _balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 _balance1 = IERC20(token1).balanceOf(address(this));
+        uint256 _amount0 = _balance0 - _reserve0;
+        uint256 _amount1 = _balance1 - _reserve1;
+
+        uint256 _totalSupply = totalSupply;
+
+        if (_totalSupply == 0) {
+            // First deposit - lock minimum liquidity
+            liquidity = uint256(sqrt(_amount0 * _amount1)) - MINIMUM_LIQUIDITY;
+            totalSupply = MINIMUM_LIQUIDITY;
+            // Permanently lock MINIMUM_LIQUIDITY tokens
+            balanceOf[address(0)] = MINIMUM_LIQUIDITY;
+        } else {
+            // Subsequent deposits
+            uint256 liquidity1 = (_amount0 * _totalSupply) / _reserve0;
+            uint256 liquidity2 = (_amount1 * _totalSupply) / _reserve1;
+            liquidity = liquidity1 < liquidity2 ? liquidity1 : liquidity2;
+        }
+
+        require(liquidity > 0, "INSUFFICIENT_LIQUIDITY_MINTED");
+        
+        balanceOf[msg.sender] += liquidity;
+        totalSupply += liquidity;
+
+        reserve0 = _balance0;
+        reserve1 = _balance1;
+
+        IERC20(token0).safeTransferFrom(msg.sender, address(this), _amount0);
+        IERC20(token1).safeTransferFrom(msg.sender, address(this), _amount1);
+
+        emit Deposit(msg.sender, _amount0, _amount1);
     }
-    
-    function removeLiquidity(address to, uint256 liquidity) public returns (uint256 amount0, uint256 amount1) {
-        // Implementation would go here
+
+    function removeLiquidity(uint256 liquidity) external nonReentrant returns (uint256 amount0, uint256 amount1) {
+        require(liquidity > 0, "INSUFFICIENT_LIQUIDITY_BURNED");
+        
+        // Use internal reserves instead of balanceOf(address(this))
+        uint256 _totalSupply = totalSupply;
+        amount0 = (liquidity * reserve0) / _totalSupply;
+        amount1 = (liquidity * reserve1) / _totalSupply;
+
+        require(amount0 > 0 && amount1 > 0, "INSUFFICIENT_LIQUIDITY_BURNED");
+
+        balanceOf[msg.sender] -= liquidity;
+        totalSupply -= liquidity;
+
+        // Update reserves before transfer
+        reserve0 -= amount0;
+        reserve1 -= amount1;
+
+        IERC20(token0).safeTransfer(msg.sender, amount0);
+        IERC20(token1).safeTransfer(msg.sender, amount1);
+
+        // Sync reserves to actual balances in case of direct transfers
+        sync();
+
+        emit Withdraw(msg.sender, amount0, amount1);
     }
-    
-    function swap(address to, uint256 amount0In, uint250 amount1In) public {
-        // Implementation would go here
-    }
-    
+
     function sync() public {
-        // Implementation would go here
+        uint256 _balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 _balance1 = IERC20(token1).balanceOf(address(this));
+        
+        reserve0 = _balance0;
+        reserve1 = _balance1;
+        
+        emit Sync(reserve0, reserve1);
     }
-    
-    // Additional helper functions and events would be defined here
+
+    // Helper function for sqrt calculation
+    function sqrt(uint256 x) internal pure returns (uint256 y) {
+        uint256 z = (x + 1) / 2;
+        y = x;
+        while (z < y) {
+            y = z;
+            z = (x / z + z) / 2;
+        }
+    }
 }
 }
