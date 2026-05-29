74e6bde63cb245d923c37b3a27be55149ac67bec
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-28 22:49:45 -0400

    fix: apply solution for issue #917

---
+++ solidity/contracts/contracts/TokenVesting.sol
@@ -0,0 +1,17 @@
+        require(released);
+        return;
+    }
+    uint256 vested = (totalAllocation * elapsed) / duration;
+    require(released);
+    return;
+}
+    uint256 unvested = totalAllocation - (totalAllocation * elapsed / duration);
+    return;
+  }
+  uint256 unvested = totalAllocation - (totalAllocation * elapsed) / duration - (totalAllocation * elapsed / duration);
+  return;
+}
+    uint256 unvested = totalAllocation - (totalAllocation * elapsed) / duration;
+    uint256 unvested = totalAllocation - totalAllocation * elapsed / duration;
+    return;
+}
