ffc0e0bfe54b14159df6a93de08044edf4667c52
Errordog2 <zmaxzhao1996@gmail.com>
2026-06-13 08:45:18 +0800

    feat(laravel): add webhook delivery system

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('webhooks')->group(function (): void {
+    Route::get('/', [WebhookController::class, 'index']);
+    Route::post('/', [WebhookController::class, 'store']);
+    Route::put('/{webhook}', [WebhookController::class, 'update']);
+    Route::delete('/{webhook}', [WebhookController::class, 'destroy']);
 });
