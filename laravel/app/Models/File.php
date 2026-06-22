ea39dc8cf3a90193b53b9a1d734682ed73f4cd04
SKYJAMES777 <3886190@qq.com>
2026-06-22 22:34:57 +0800

    [ Laravel ] Add File model

---
+++ laravel/app/Models/File.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class File extends Model
+{
+    protected $table = "files";
+
+    protected $fillable = [
+        "original_name", "stored_path", "mime_type",
+        "size_bytes", "checksum_sha256", "uploaded_by",
+        "thumbnail_path",
+    ];
+
+    public static function findByChecksum(string $checksum): ?self
+    {
+        return self::where("checksum_sha256", $checksum)->first();
+    }
+}
