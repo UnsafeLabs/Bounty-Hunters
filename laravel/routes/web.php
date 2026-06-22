4bffb9fed12a7b04fcdc78e0aac173147805f5b4
SKYJAMES777 <3886190@qq.com>
2026-06-22 21:58:11 +0800

    fix(laravel): add rate limiting to web routes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::middleware('throttle:60,1')->group(function () {
+    Route::get('/', function () {
     return view('welcome');
 });
+});
+
+Route::get('/rate-limit-debug', function (\Illuminate\Http\Request $request) {
+    return response()->json([
+        'path' => $request->path(),
+        'method' => $request->method(),
+        'ip' => $request->ip(),
+    ]);
+})->middleware('throttle:60,1');
