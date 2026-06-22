bcb133aea91b7071d1e48d65e7055dd32ce65147
SKYJAMES777 <3886190@qq.com>
2026-06-22 22:06:49 +0800

    [ Laravel ] Add CustomVerifyEmail notification

---
+++ laravel/app/Notifications/CustomVerifyEmail.php
@@ -0,0 +1,27 @@
+﻿<?php
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class CustomVerifyEmail extends VerifyEmail
+{
+    use Queueable;
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $verificationUrl = $this->verificationUrl($notifiable);
+
+        return (new MailMessage)
+            ->subject('Verify Your Email Address')
+            ->view('emails.verify-email', [
+                'user' => $notifiable,
+                'verificationUrl' => $verificationUrl,
+            ]);
+    }
+}
