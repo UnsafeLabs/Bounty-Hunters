fbce8c4f500fac5d6663deb23c7ddde45a0e9095
SKYJAMES777 <3886190@qq.com>
2026-06-22 22:07:33 +0800

    [ Laravel ] Add email verification routes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return view("welcome");
+});
+
+Route::middleware("guest")->group(function () {
+    Route::get("/email/verify", function () {
+        return view("auth.verify-email");
+    })->name("verification.notice");
+
+    Route::post("/email/verification-notification", [\App\Http\Controllers\Auth\EmailVerificationController::class, "resend"])
+        ->middleware(["throttle:1,1"])
+        ->name("verification.send");
+
+    Route::get("/email/verify/{id}/{hash}", [\App\Http\Controllers\Auth\EmailVerificationController::class, "verify"])
+        ->middleware(["signed"])
+        ->name("verification.verify");
 });
