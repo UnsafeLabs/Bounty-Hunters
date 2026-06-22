39d10e5a4d8c20c2838b89c093f2e9a447e20d15
SKYJAMES777 <3886190@qq.com>
2026-06-22 22:15:20 +0800

    [ Laravel ] Register LogFailedJob listener

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Queue\Events\JobFailed;
+use App\Listeners\LogFailedJob;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \Event::listen(JobFailed::class, [LogFailedJob::class, "handle"]);
     }
 }
