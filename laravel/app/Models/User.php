4d76cc07eeae01f25a3b01195769a516a871e77c
SKYJAMES777 <3886190@qq.com>
2026-06-22 22:06:23 +0800

    [ Laravel ] Implement MustVerifyEmail interface on User model

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -1,8 +1,8 @@
-<?php
+﻿<?php
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
@@ -12,7 +12,7 @@ use Illuminate\Notifications\Notifiable;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
