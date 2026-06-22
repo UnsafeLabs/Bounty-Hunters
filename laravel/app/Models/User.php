f5dd3657339433d82798b3ec35e6e079ebffee6e
SKYJAMES777 <3886190@qq.com>
2026-06-22 22:16:56 +0800

    [ Laravel ] Apply ActiveScope to User model

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -9,6 +9,8 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Scopes\ActiveScope;
+use Illuminate\Database\Eloquent\Attributes\GlobalScope;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
