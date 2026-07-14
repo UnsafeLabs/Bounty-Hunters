cc44a7f2c32c5c3cfb7c8286ed6c38de0723c824
ardha27 <ardha27@users.noreply.github.com>
2026-07-14 19:00:28 +0800

    fix: Use configurable bcrypt rounds in User password mutator (#745)

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
@@ -26,7 +27,15 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Set the user's password with bcrypt rounds from config.
+     */
+    public function setPasswordAttribute(string $value): void
+    {
+        $rounds = config('hashing.bcrypt.rounds', 10);
+        $this->attributes['password'] = Hash::make($value, ['rounds' => (int) $rounds]);
     }
 }
